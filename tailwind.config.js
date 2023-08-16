3db58e1c8e2e062b6bc3639c06f761b494e737e8
Jaka Potokar <cc.jakapotokar@gmail.com>
2023-08-16 12:43:43 +0200

    updated paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     screens: {
-      sm: "480px",
+      sm: "240px",
       md: "768px",
       lg: "976px",
       xl: "1440px",
