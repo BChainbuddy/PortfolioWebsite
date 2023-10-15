2f5f609fed5a67e9a4abf9aec7446146605b9e80
Jaka Potokar <cc.jakapotokar@gmail.com>
2023-10-15 16:22:00 +0200

    updated the work section

--- build/js/button.js
+++ build/js/button.js
@@ -6,3 +6,27 @@ btn.addEventListener("click", () => {
   nav.classList.toggle("flex");
   nav.classList.toggle("hidden");
 });
+
+const btn2 = document.getElementById("view-button");
+const nav2 = document.getElementById("work");
+const text = document.getElementById("view-more");
+const text2 = document.getElementById("view-less");
+const btn2div = document.getElementById("btn-div");
+
+btn2.addEventListener("click", () => {
+  //SHOW ALL
+  nav2.classList.toggle("lg:flex");
+  nav2.classList.toggle("lg:hidden");
+  //VIEW MORE
+  text.classList.toggle("hidden");
+  text.classList.toggle("flex");
+  //VIEW LESS
+  text2.classList.toggle("flex");
+  text2.classList.toggle("hidden");
+  //CHANGE BUTTON CLASS
+  if (btn2div.classList.contains("pb-6")) {
+    btn2div.classList.remove("pb-6");
+  } else {
+    btn2div.classList.add("pb-6");
+  }
+});
