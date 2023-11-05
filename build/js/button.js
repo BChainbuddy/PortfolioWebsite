da021b0dc615ed6c65d5abf5b2f8053f0e3da947
Jaka Potokar <cc.jakapotokar@gmail.com>
2023-11-05 17:03:13 +0100

    added Filter

--- build/js/button.js
+++ build/js/button.js
@@ -13,6 +13,11 @@ const nav3 = document.getElementById("work2");
 const text = document.getElementById("view-more");
 const text2 = document.getElementById("view-less");
 const btn2div = document.getElementById("btn-div");
+const recentlyAdded = document.getElementById("recentlyAdded");
+const allProjects = document.getElementById("allProjects");
+const filterMenu = document.getElementById("filterMenu");
+const filterButton = document.getElementById("filterButton");
+const filter = document.getElementById("filter");
 
 btn2.addEventListener("click", () => {
   //SHOW ALL
@@ -20,16 +25,50 @@ btn2.addEventListener("click", () => {
   nav2.classList.toggle("lg:hidden");
   nav3.classList.toggle("lg:flex");
   nav3.classList.toggle("lg:hidden");
+  //RECENTLY ADDED AND ALL PROJECTS
+  recentlyAdded.classList.toggle("hidden");
+  recentlyAdded.classList.toggle("flex");
+  allProjects.classList.toggle("flex");
+  allProjects.classList.toggle("hidden");
+  //WHOLEFILTER
+  filter.classList.toggle("hidden");
   //VIEW MORE
-  text.classList.toggle("hidden");
   text.classList.toggle("flex");
+  text.classList.toggle("hidden");
   //VIEW LESS
   text2.classList.toggle("flex");
   text2.classList.toggle("hidden");
   //CHANGE BUTTON CLASS
-  if (btn2div.classList.contains("pb-6")) {
-    btn2div.classList.remove("pb-6");
+  if (btn2div.classList.contains("pb-10")) {
+    btn2div.classList.remove("pb-10");
   } else {
-    btn2div.classList.add("pb-6");
+    btn2div.classList.add("pb-10");
+  }
+});
+
+//FILTER
+
+const filterJavascript = document.getElementById("filterJavascript");
+const filterPython = document.getElementById("filterPython");
+const filterSolidity = document.getElementById("filterSolidity");
+const filterNextjs = document.getElementById("filterNextjs");
+const filterHardat = document.getElementById("filterHardhat");
+
+const javascriptContent = document.getElementsByClassName("javascript");
+
+filterButton.addEventListener("click", () => {
+  filterMenu.classList.toggle("hidden");
+  filterMenu.classList.toggle("flex");
+  filterButton.classList.toggle("rounded-lg");
+  filterButton.classList.toggle("rounded-t-lg");
+  filterMenu.classList.toggle("rounded-lg");
+  filterMenu.classList.toggle("rounded-b-lg");
+});
+
+filterJavascript.addEventListener("click", () => {
+  for (let i = 0; i < javascriptContent.length; i++) {
+    if (javascriptContent[i].classList.contains("border-8")) {
+      javascriptContent[i].classList.toggle("bg-white");
+    }
   }
 });
