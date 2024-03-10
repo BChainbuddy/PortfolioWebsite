5c7bc9ed716a07d15931f4f21dbe5a988106ac70
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-03-10 22:52:46 +0100

    updates to header and new project links

--- build/js/observeScroll.js
+++ build/js/observeScroll.js
@@ -15,21 +15,17 @@ bounds = header.getBoundingClientRect().top;
 
 document.addEventListener("scroll", () => {
   // console.log(bounds);
-  if (window.scrollY > bounds + 6) {
+  if (window.scrollY > bounds) {
     //flex flex-row justify-between sm:items-center sm:pb-4 pt-4 lg:pb-0 border-bottom-LightBlue md:border-b-0 sm:bg-teal-950 md:bg-transparent
     //Change p, change bg
     header.classList.remove("lg:pb-0");
     header.classList.add("py-8");
-    header.classList.remove("md:bg-transparent");
-    header.classList.add("sticky", "top-0");
-    header.classList.remove("md:border-b-0");
+    header.classList.remove("lg:bg-transparent");
+    header.classList.remove("lg:border-b-0");
   } else {
-    if (header.classList.contains("sticky")) {
-      header.classList.remove("py-8");
-      header.classList.add("lg:pb-0");
-      header.classList.add("md:bg-transparent");
-      header.classList.add("md:border-b-0");
-      header.classList.remove("sticky", "top-0");
-    }
+    header.classList.remove("py-8");
+    header.classList.add("lg:pb-0");
+    header.classList.add("lg:bg-transparent");
+    header.classList.add("lg:border-b-0");
   }
 });
